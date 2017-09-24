0021452e13ea64f7f4e0b64839c3310f0c625d20
sizikoff <sizikoffps@mail.ru>
2017-09-24 11:23:48 +0300

    Расширен диапазон генерации до 100,добавлен графический интерфейс Swing

--- src/com/company/Number.java
+++ src/com/company/Number.java
@@ -1,12 +1,58 @@
 package com.company;
 
-public class Number {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-    public static void main(String[] args) {
-        int a = 0;// Начальное значение диапазона - "от"
-        int b = 10;// Конечное значение диапазона - "до"
+public class Number extends JFrame {
+    public Number(){
+        setTitle("ГСЧ");
+        setSize(170,100);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        int random_number = a + (int) (Math.random() * b); // Генерация
-        System.out.println(random_number);
+        setLayout(new FlowLayout());
+        JButton jbt = new JButton("Клик");
+        add(jbt);
+
+        JButton jbt2 = new JButton("Выход");
+        add(jbt2);
+
+        JTextField jtf = new JTextField("Число");
+        add(jtf);
+        jtf.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if ( ((c < '0') || (c > '9'))) {
+                    e.consume();  // игнорим введенные буквы и пробел
+                }
+            }
+        });
+
+        jbt2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        jbt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int a = 0;
+                int b = 100;
+
+                int random_number = a + (int) (Math.random() * b);
+                String aString = Integer.toString(random_number);
+                jtf.setText(aString);
+            }
+        });
+
+        this.setResizable(false);
+        setVisible(true);
     }
+
 }
